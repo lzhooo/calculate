f4a270ef968718158849952ea3e756e2109bf4ea
lzhoo <lizhiheng@geekhacker.tech>
2018-09-25 22:13:09 +0800

    完成功能

--- myapp.py
+++ myapp.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import itertools
+from fractions import  Fraction
 def CalculateExpress(num):
     symbols = ['+', '-', '*', '/']
     expression = [str(random.randint(1, num))]
@@ -76,24 +77,33 @@ def caculate(expression):
             num1=num.pop()
             num2=num.pop()
             try:
-                num.append(str(eval("%s%s%s"%(num2,i,num1))))
+                if i =='+':
+                    num.append(Fraction(num2)+Fraction(num1))
+                if i =='-':
+                    num.append(Fraction(num2)-Fraction(num1))
+                if i =='*':
+                    num.append(Fraction(num2)*Fraction(num1))
+                if i =='/':
+                    num.append(Fraction(num2)/Fraction(num1))
             except:
                 return "null"
-    return round(float(num[0]),1)
+    return Fraction(num[0])
 
 def CalculateNum(num):
-    result = []
-    f1=open('a.txt','w')
+    f1=open('a.txt','w+')
     f2=open('answer.txt','w')
     i=1
     while i <= int(num):
         a = CalculateExpress(10)
-        f1.write(str(i)+'.  '+''.join(a)+'\n')
-        nbl = RemoveRe(a)
-        answer=caculate(nbl)
-        f2.write(str(answer)+'\n')
-        result.append(''.join(a))
-        i+=1
+        if a not in f1.readlines():
+            nbl = RemoveRe(a)
+            answer=caculate(nbl)
+            if answer =='null' or '-' in str(answer):
+                pass
+            else:
+                f1.write(str(i) + '.  ' + ''.join(a) + '\n')
+                f2.write(str(answer)+'\n')
+                i+=1
     f1.close()
     f2.close()
 
@@ -105,14 +115,13 @@ def CalculateCheck(file_a,file_c):
     f3=open(file_c,'r').readlines()
     wrong_list=[]
     right_list=[]
-    print(len(f3))
     for i in range(0,len(f3)):
         if f2[i]!=f3[i]:
             wrong_list.append(i+1)
         else:
             right_list.append(i+1)
-    print('总共答对'+str(len(right_list))+'题\n'+'正确的是:'+str(right_list)+'\n错误的是:'+str(wrong_list))
-
+    f4=open('grade.txt','w')
+    f4.write('总共答对'+str(len(right_list))+'题\n'+'正确的是:'+str(right_list)+'\n错误的是:'+str(wrong_list))
 
 
 def main():
@@ -121,7 +130,7 @@ def main():
     elif sys.argv[1] =='-r':
         CalculateRange(sys.argv[2])
     elif sys.argv[1] =='-e':
-        CalculateCheck( sys.argv[2], sys.argv[3])
+        CalculateCheck( sys.argv[2], sys.argv[4])
     else:
         print("输入格式有误")
     sys.exit(1)
